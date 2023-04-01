1b0143010a08965508cfda62e1212c295616894d
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-04-01 13:01:10 +0600

    load fake data using useEffect

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Blog.css';
 
 const Blog = () => {
+
+    const [info, setInfo] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setInfo(data));
+    },[]);
+
     return (
         <div className='blog-container'>
             <div className="main-blog">
-                <h1>Main</h1>
+                <h1>total: {info.length}</h1>
             </div>
             <div className="blog-detail">
                 <h1>Detail</h1>

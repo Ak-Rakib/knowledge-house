693fe36492c37466ec8d2d92b584bbb580c0b358
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-04-01 17:33:01 +0600

    cover image and profile photo

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
-    const {name} = props.i;
+    const {name, profile, image, Date, title, Time} = props.i;
+    console.log(image);
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className='cart-container'>
+            <div className="cover-img-p">
+               <img className='cover-img' src={image} alt="" />
+            </div>
+            <div className="profile-parents">
+                <div className="profile-child01">
+                        <img className='inside-profile' src={profile} alt="" />
+                    <div className="profile-details">
+                        <h4 className='profile-name'>{name}</h4>
+                        <span className='date-style'>{Date}</span>
+                    </div>
+                </div>
+                <div className="time-child02">
+                    <p className='time-style'>0<span>{Time}</span> min read</p> 
+                </div>
+            </div>
         </div>
     );
 };
